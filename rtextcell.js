6b06ea84c0b1dd1f874287f55d95a477a370f665
Samuel <samuvalcarcel@gmail.com>
2017-04-02 11:27:45 +0100

    Completadas las clases en ECMA6

--- rtextcell.js
+++ rtextcell.js
@@ -1,22 +1,7 @@
 var tc = require("./textcell.js");
 var TextCell = tc.TextCell;
 
-/* RTextCell
-function RTextCell(text) {
-  TextCell.call(this, text);
-}
-RTextCell.prototype = Object.create(TextCell.prototype);
-RTextCell.prototype.draw = function(width, height) {
-  var result = [];
-  for (var i = 0; i < height; i++) {
-    var line = this.text[i] || "";
-    result.push(" ".repeat(width - line.length) + line);
-  }
-  return result;
-};
- End RTextCell */
-
- class RTextCell extends TextCell {
+class RTextCell extends TextCell {
    constructor(text) {
      super(text);
    }
